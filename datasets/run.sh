0c64e056ab453cdb2026857f35bf4c726f4acd2f
ChengJiacheng <31717437+ChengJiacheng@users.noreply.github.com>
2020-11-05 02:04:15 -0800

    none

---
+++ datasets/run.sh
@@ -0,0 +1,4 @@
+apt-get install -y zip
+wget https://gitlab.nautilus.optiputer.net/jiacheng/nautilus/-/blob/master/datasets/tinyimagenet200.zip.001
+wget https://gitlab.nautilus.optiputer.net/jiacheng/nautilus/-/blob/master/datasets/tinyimagenet200.zip.002
+wget https://gitlab.nautilus.optiputer.net/jiacheng/nautilus/-/blob/master/datasets/tinyimagenet200.zip.003
